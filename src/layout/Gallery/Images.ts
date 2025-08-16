c77b95797f013e0e9401ff24c342fbf438f97282
kimjiun <ggkimh@hanmail.net>
2025-08-16 23:15:36 +0900

    이미지에 맞춰 가로 세로값변경

--- src/layout/Gallery/Images.ts
+++ src/layout/Gallery/Images.ts
@@ -1,95 +1,96 @@
 import data from 'data.json';
 
-const portaraitWidth = 683;
-const portaraitHeight = 1024;
-const landScapeWidth = 1023;
-const landScapeHeight = 683;
-
 const images = [
   /* 이미지 경로를 리스트로 저장 */
   {
     alt: 'image02',
     source: `${data.bucketUrl}02.jpg`,
-    width: portaraitWidth,
-    height: portaraitHeight,
+    width: 1350,
+    height: 2025,
   },
   {
     alt: 'image03',
     source: `${data.bucketUrl}03.jpg`,
-    width: portaraitWidth,
-    height: portaraitHeight,
+    width: 1350,
+    height: 2025,
   },
   {
     alt: 'image04',
     source: `${data.bucketUrl}04.jpg`,
-    width: portaraitWidth,
-    height: portaraitHeight,
+    width: 1350,
+    height: 2025,
   },
   {
     alt: 'image05',
     source: `${data.bucketUrl}05.jpg`,
-    width: portaraitWidth,
-    height: portaraitHeight,
+    width: 2160,
+    height: 1440,
   },
   {
     alt: 'image06',
     source: `${data.bucketUrl}06.jpg`,
-    width: portaraitWidth,
-    height: portaraitHeight,
+    width: 1350,
+    height: 2025,
   },
   {
     alt: 'image07',
     source: `${data.bucketUrl}07.jpg`,
-    width: portaraitWidth,
-    height: portaraitHeight,
+    width: 1350,
+    height: 2025,
   },
   {
     alt: 'image08',
     source: `${data.bucketUrl}08.jpg`,
-    width: portaraitWidth,
-    height: portaraitHeight,
+    width: 1334,
+    height: 2002,
   },
   {
     alt: 'image09',
     source: `${data.bucketUrl}09.jpg`,
-    width: portaraitWidth,
-    height: portaraitHeight,
+    width: 1350,
+    height: 2025,
   },
   {
     alt: 'image10',
     source: `${data.bucketUrl}10.jpg`,
-    width: landScapeWidth,
-    height: landScapeHeight,
+    width: 1350,
+    height: 2025,
   },
   {
     alt: 'image11',
     source: `${data.bucketUrl}11.jpg`,
-    width: portaraitWidth,
-    height: portaraitHeight,
+    width: 1350,
+    height: 2025,
   },
   {
     alt: 'image12',
     source: `${data.bucketUrl}12.jpg`,
-    width: portaraitWidth,
-    height: portaraitHeight,
+    width: 1350,
+    height: 2025,
   },
   {
     alt: 'image13',
     source: `${data.bucketUrl}13.jpg`,
-    width: portaraitWidth,
-    height: portaraitHeight,
+    width: 1350,
+    height: 2025,
   },
   {
     alt: 'image14',
     source: `${data.bucketUrl}14.jpg`,
-    width: portaraitWidth,
-    height: portaraitHeight,
+    width: 1350,
+    height: 2025,
   },
   {
     alt: 'image15',
     source: `${data.bucketUrl}15.jpg`,
-    width: portaraitWidth,
-    height: portaraitHeight,
+    width: 1620,
+    height: 2064,
+  },
+  {
+    alt: 'image16',
+    source: `${data.bucketUrl}16.jpg`,
+    width: 1500,
+    height: 1800,
   },
 ];
 
