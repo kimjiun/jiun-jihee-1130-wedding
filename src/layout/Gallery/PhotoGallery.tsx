877c2006c63f6213a31653a1dda65b2b283e2f2e
kimjiun <ggkimh@hanmail.net>
2025-08-17 17:49:12 +0900

    사진 상세 팝업 노출중 뒤로가기 클릭시 팝업 닫히게 처리 1. onBeforeOpen 콜백 등록하여 PhotoSwipe 객체 획득 2. 뒤로가기 버튼 클릭시 PhotoSwipe 객체의 isOpen 값 확인 후 close

--- src/layout/Gallery/PhotoGallery.tsx
+++ src/layout/Gallery/PhotoGallery.tsx
@@ -1,6 +1,8 @@
 import { Gallery, Item } from 'react-photoswipe-gallery';
 import 'photoswipe/style.css';
 import images from '@/layout/Gallery/Images.ts';
+import { useEffect, useRef } from 'react';
+import PhotoSwipe from 'photoswipe';
 
 const PhotoGallery = () => {
   const smallItemStyles: React.CSSProperties = {
@@ -9,9 +11,38 @@ const PhotoGallery = () => {
     width: '26vw',
     height: '39vw',
   };
+  const photoswipeRef = useRef<PhotoSwipe | null>(null);
+
+  useEffect(() => {
+    const onPopState = () => {
+      if (photoswipeRef.current && photoswipeRef.current.isOpen) {
+        photoswipeRef.current.close();
+      }
+    };
+
+    window.addEventListener('popstate', onPopState);
+
+    return () => {
+      window.removeEventListener('popstate', onPopState);
+    };
+  }, []);
+
+  const handleBeforeOpen = (pswp: PhotoSwipe) => {
+    photoswipeRef.current = pswp;
+
+    pswp.on('afterInit', () => {
+      window.history.pushState({ photoswipe: true }, '');
+    });
+
+    pswp.on('close', () => {
+      if (window.history.state?.photoswipe) {
+        window.history.back();
+      }
+    });
+  };
 
   return (
-    <Gallery>
+    <Gallery withCaption onBeforeOpen={handleBeforeOpen}>
       <div
         style={{
           display: 'grid',
