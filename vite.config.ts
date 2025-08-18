3baf626439c309152d94e97104387e4ef9e41a89
kimjiun <ggkimh@hanmail.net>
2025-08-18 14:01:56 +0900

    신랑, 신부 부모님용 og tag 포함된 index.html 파일 생성

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
@@ -6,5 +7,16 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
-  base:"/jiun-jihee-1130-wedding/"
+  base: '/jiun-jihee-1130-wedding/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        groom: resolve(__dirname, 'groom.html'),
+        bride: resolve(__dirname, 'bride.html'),
+        bride_dad: resolve(__dirname, 'bride_dad.html'),
+        bride_mom: resolve(__dirname, 'bride_mom.html'),
+      },
+    },
+  },
 });
